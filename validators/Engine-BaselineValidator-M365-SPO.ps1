ed7ff740f64d7533b8af4f0f4260d46ef9b047e2
Tobias Maestrini <tobias@bee365.ch>
2024-03-07 00:37:35 +0100

    fix validator

--- validators/Engine-BaselineValidator-M365-SPO.ps1
+++ validators/Engine-BaselineValidator-M365-SPO.ps1
@@ -1,20 +1,39 @@
 #Parameters
-$AdminCenterURL = "https://[tenant]-admin.sharepoint.com"
+$AdminCenterURL = "https://tmaestrini-admin.sharepoint.com"
 Connect-PnPOnline -Url $AdminCenterURL -Interactive
 
 $fileContent = Get-Content "./baselines/M365.SPO-5.2.yml" -Raw
 $baseline = ConvertFrom-Yaml $fileContent -AllDocuments
 
 if ($baseline.Topic -eq "SharePoint Online") {
-    $tenantSettings = Get-PnPTenant | Select-Object ($baseline.Configuration.ExternalSharing.Keys)
-    $baselineSettings = $baseline.Configuration.ExternalSharing
-
-    $output = [System.Collections.SortedList]::new()
-    foreach ($key in $baselineSettings.Keys) {
-        Write-Host $key
-        $test = $null -ne $tenantSettings.$key ? (Compare-Object -ReferenceObject $baselineSettings.$key -DifferenceObject $tenantSettings.$key -IncludeEqual) : $null
-        if ($test) { $output.Add($key, $test.SideIndicator -eq "==" ? "✔︎ [$($tenantSettings.$key)]" : "✘ [Should be '$($baselineSettings.$key)' but is '$($tenantSettings.$key)']") | Out-Null }
-        else { $output.Add($key, "---") | Out-Null }
+    $tenantSettings = Get-PnPTenant 
+    Clear-Host
+    Write-Host "`n------------------------------`n⭐︎ Baseline Validation Results`n------------------------------"
+    Test-Settings $tenantSettings -Baseline $baseline | Sort-Object -Property Group, Key `
+    | Format-Table -GroupBy Group -Wrap -Property Setting, Result
+    
+    function Test-Settings($tenantSettings, $baseline) {
+        $output = @();
+        foreach ($baselineSettingsGroup in $baseline.Configuration.Keys) {
+            foreach ($key in $baseline.Configuration[$baselineSettingsGroup].Keys) {
+                $setting = $baseline.Configuration[$baselineSettingsGroup]
+                $test = $null -ne $tenantSettings.$key ? (Compare-Object -ReferenceObject $setting.$key -DifferenceObject $tenantSettings.$key -IncludeEqual) : $null
+                if ($test) { 
+                    $output += [PSCustomObject]@{
+                        Group   = $baselineSettingsGroup
+                        Setting = $key
+                        Result  = $test.SideIndicator -eq "==" ? "✔︎ [$($tenantSettings.$key)]" : "✘ [Should be '$($setting.$key)' but is '$($tenantSettings.$key)']"
+                    }
+                }
+                else { 
+                    $output += [PSCustomObject]@{
+                        Group   = $baselineSettingsGroup
+                        Setting = $key
+                        Result  = "---"
+                    }
+                } 
+            }
+        }
+        return $output
     }
-    Write-Output ($output | Sort-Object -Property Key)
 }
