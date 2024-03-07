278cfb69917a7193e62c6bc64a2a52a64252af62
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2024-03-07 09:03:43 -0500

    Added logic for tenant variables

--- validators/Engine-BaselineValidator-M365-SPO.ps1
+++ validators/Engine-BaselineValidator-M365-SPO.ps1
@@ -1,6 +1,11 @@
+# Set things up
+Import-Module ./utilities/UtilityFunctions.psm1 -Force
+
+Add-TenantVariables
+
+
 #Parameters
-$AdminCenterURL = "https://[tenant]-admin.sharepoint.com"
-Connect-PnPOnline -Url $AdminCenterURL -Interactive
+Connect-PnPOnline -Url $adminSiteUrl -Interactive
 
 $fileContent = Get-Content "./baselines/M365.SPO-5.2.yml" -Raw
 $baseline = ConvertFrom-Yaml $fileContent -AllDocuments
@@ -12,28 +17,4 @@ if ($baseline.Topic -eq "SharePoint Online") {
     Test-Settings $tenantSettings -Baseline $baseline | Sort-Object -Property Group, Key `
     | Format-Table -GroupBy Group -Wrap -Property Setting, Result
     
-    function Test-Settings($tenantSettings, $baseline) {
-        $output = @();
-        foreach ($baselineSettingsGroup in $baseline.Configuration.Keys) {
-            foreach ($key in $baseline.Configuration[$baselineSettingsGroup].Keys) {
-                $setting = $baseline.Configuration[$baselineSettingsGroup]
-                $test = $null -ne $tenantSettings.$key ? (Compare-Object -ReferenceObject $setting.$key -DifferenceObject $tenantSettings.$key -IncludeEqual) : $null
-                if ($test) { 
-                    $output += [PSCustomObject]@{
-                        Group   = $baselineSettingsGroup
-                        Setting = $key
-                        Result  = $test.SideIndicator -eq "==" ? "✔︎ [$($tenantSettings.$key)]" : "✘ [Should be '$($setting.$key)' but is '$($tenantSettings.$key)']"
-                    }
-                }
-                else { 
-                    $output += [PSCustomObject]@{
-                        Group   = $baselineSettingsGroup
-                        Setting = $key
-                        Result  = "---"
-                    }
-                } 
-            }
-        }
-        return $output
-    }
 }
