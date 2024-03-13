c5bcd12290841d4115b80e140eb95bf9f918833d
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-03-13 22:17:17 +0100

    add function to get baseline template from structure

--- src/utilities/TemplateFunctions.psm1
+++ src/utilities/TemplateFunctions.psm1
@@ -31,3 +31,38 @@ function Get-TenantTemplate {
     return $settings
   }
 }
+
+<#
+.Synopsis
+.DESCRIPTION
+.EXAMPLE
+   Get-BaselineTemplate
+#>
+function Get-BaselineTemplate {
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType([int])]
+  Param
+  (
+    [Parameter(
+      Mandatory = $true
+    )][string]$BaselineId,
+    [Parameter(Mandatory = $true)]
+    [string]$BaselinesFolder
+  )
+ 
+  Begin {
+    $ConfigPath = Join-Path -Path $BaselinesFolder -ChildPath "$($BaselineId.Trim()).yml"
+    
+  }
+  Process {
+    if (-not(Test-Path -Path $ConfigPath -PathType Leaf)) {
+      Write-Error "No template found for '$BaselineId'" -ErrorAction Stop
+    }
+    $fileContent = Get-Content $ConfigPath -Raw -ErrorAction Stop
+    $baseline = ConvertFrom-Yaml $fileContent -AllDocuments
+  }
+  End {
+    return $baseline
+  }
+}
