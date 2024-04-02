1d36846e65ed40a6a00dbf97d7ddc17f06e94206
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-04-02 09:33:18 +0200

    add logging initialization

---
+++ src/utilities/CommonFunctions.psm1
@@ -0,0 +1,11 @@
+<#
+.Synopsis
+.DESCRIPTION
+.EXAMPLE
+   Get-TenantTemplate
+#>
+Function Initialize-Logging() {
+  Set-LoggingDefaultLevel -Level 'INFO'
+  Add-LoggingTarget -Name Console
+  Add-LoggingTarget -Name File -Configuration @{Path = (Join-Path -Path $PSScriptRoot -ChildPath '../../logs/Provisioning_%{+%Y%m%d}.log') }      
+}
