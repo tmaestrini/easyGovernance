6db1892bd453a90a2ef29c1bce034ea278f1409a
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2024-04-02 09:18:28 -0400

    Reverted example changes

--- examples/example-validation-data.ps1
+++ examples/example-validation-data.ps1
@@ -1,3 +1,3 @@
 # Validate a given tenant from settings file and store the result in a variable
 Import-Module .\src\Validation.psm1 -Force
-$result = Start-Validation -TemplateName "settings_template.yml" -ReturnAsObject
+$result = Start-Validation -TemplateName "[tenantname].yml" -ReturnAsObject
--- examples/example-validation.ps1
+++ examples/example-validation.ps1
@@ -1,3 +1,3 @@
 # Validate a given tenant from settings file
 Import-Module .\src\Validation.psm1 -Force
-Start-Validation -TemplateName "settings_template.yml" > output.md
+Start-Validation -TemplateName "[tenantname].yml" > output.md
