ff7419ae4a9698240d07ceb7a773b51974fff2e6
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-04-24 23:16:38 +0200

    adjust baseline retrieval routine

--- src/utilities/TemplateFunctions.psm1
+++ src/utilities/TemplateFunctions.psm1
@@ -50,19 +50,29 @@ function Get-BaselineTemplate {
   )
  
   Begin {
-    $ConfigPath = Join-Path -Path $PSScriptRoot -ChildPath ../../baselines/$($BaselineId.Trim()).yml
-    
+    if ($null -eq $Script:baselines) {
+      $ConfigPath = Join-Path -Path $PSScriptRoot -ChildPath ..\..\baselines
+      $BaselinesFiles = @( Get-ChildItem -Path $ConfigPath\*.yml -ErrorAction SilentlyContinue )
+
+      $Script:baselines = @()
+      foreach ($BaselineFile in $BaselinesFiles) {
+        $fileContent = Get-Content $BaselineFile.FullName -Raw -ErrorAction Stop
+        $baseline = ConvertFrom-Yaml $fileContent -AllDocuments
+        $baseline.Filename = $BaselineFile.BaseName
+        $Script:baselines += $baseline
+      }
+    }
+    $($BaselineId.Trim()).yml    
   }
   Process {
-    if (-not(Test-Path -Path $ConfigPath -PathType Leaf)) {
-      Write-Log -Level WARNING "$($BaselineId): No baseline found at $ConfigPath" -ErrorAction Stop
+    $selectedBaseline = $Script:baselines | Where-Object { $BaselineId -eq $_.Filename -or $BaselineId -eq $_.Id }
+    if (0 -eq $selectedBaseline.Count) {
+      Write-Log -Level WARNING "$($BaselineId): No matching baseline found with id or name '$BaselineId'" -ErrorAction Stop
       throw "Baseline Error (see above)"
       return
     }
-    $fileContent = Get-Content $ConfigPath -Raw -ErrorAction Stop
-    $baseline = ConvertFrom-Yaml $fileContent -AllDocuments
   }
   End {
-    return $baseline
+    return $selectedBaseline
   }
 }
