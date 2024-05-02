dfe9e844facb73bd9ee7103a40ed9cb57940744f
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-05-02 23:39:16 +0200

    fix M365 Admin Center validation

--- src/Private/Validation/Test-M365.1-1.1.ps1
+++ src/Private/Validation/Test-M365.1-1.1.ps1
@@ -32,11 +32,20 @@ Function Test-M365.1-1.1 {
 
     function Extract() {
       try {
-        $settings = Get-M365TenantSettings -Properties "services/apps/azurespeechservices", "settings/apps/bookings", "settings/apps/calendarsharing"
+        # Office365Services
+        $settings = Get-M365TenantSettings -Properties  AzureSpeechServices, Bookings, CalendarSharing, Cortana, Copilot4Sales, M365AppsInstallationOpt, `
+          M365Lighthouse, MSGraphDataConnect, MSLoop, MSPlanner, M365Groups, M365OTW, ModernAuth, MSForms, MSTeams, MSTeamsAllowGuestAccess, MSToDo, MSUserCommunication, `
+          MSSearchBing, MSVivaBriefing, MSVivaInsights, OfficeScripts, Reports, SearchIntelligenceAnalytics, SharePoint, Sway, SwayShareWithExternalUsers, `
+          UserOwnedAppsandServices, VivaLearning, Whiteboard
+        
+        # SecurityAndPrivacy
+
+        #OrganizationProfile
+        
         return $settings
       }
       catch {
-        throw "Baseline exctraction failed: $_" 
+        Write-Log -Level CRITICAL "Baseline exctraction failed: $_" 
       } 
     }
 
@@ -44,9 +53,37 @@ Function Test-M365.1-1.1 {
       $settings = @{}
       
       # Office365Services
-      $settings.AzureSpeechServices = $extractedSettings.azurespeechservices.isTenantEnabled
-      $settings.Bookings = $extractedSettings.bookings.Enabled
-      $settings.CalendarSharing = $extractedSettings.calendarsharing.EnableCalendarSharing
+      $settings.AccountLinking = "n/a"
+      $settings.AdoptionScore = "n/a"
+      $settings.AzureSpeechServices = $extractedSettings.AzureSpeechServices
+      $settings.Bookings = $extractedSettings.Bookings
+      $settings.CalendarSharing = $extractedSettings.CalendarSharing
+      $settings.Copilot4Sales = $extractedSettings.Copilot4Sales
+      $settings.Cortana = $extractedSettings.Cortana
+      $settings.M365Groups = "needs to be specified"
+      $settings.M365AppsInstallationOpt = $extractedSettings.M365AppsInstallationOpt
+      $settings.M365Lighthouse = $extractedSettings.M365Lighthouse
+      $settings.M365OTW = $extractedSettings.M365OTW
+      $settings.MSUserCommunication = $extractedSettings.MSUserCommunication
+      $settings.MSForms = "needs to be specified"
+      $settings.MSGraphDataConnect = $extractedSettings.MSGraphDataConnect
+      $settings.MSLoop = $extractedSettings.MSLoop
+      $settings.MSPlanner = $extractedSettings.MSPlanner
+      $settings.MSSearchBing = ""
+      $settings.MSTeams = $extractedSettings.MSTeams
+      $settings.MSTeamsAllowGuestAccess = $extractedSettings.MSTeamsAllowGuestAccess
+      $settings.MSToDo = $extractedSettings.MSToDo
+      $settings.MSVivaInsights = $extractedSettings.MSVivaInsights
+      $settings.ModernAuth = $extractedSettings.ModernAuth
+      $settings.News = $extractedSettings.News
+      $settings.OfficeScripts = $extractedSettings.OfficeScripts
+      $settings.Reports = $extractedSettings.Reports
+      $settings.SearchIntelligenceAnalytics = $extractedSettings.SearchIntelligenceAnalytics
+      $settings.SharePoint = $extractedSettings.SharePoint
+      $settings.SwayShareWithExternalUsers = $extractedSettings.SwayShareWithExternalUsers
+      $settings.UserOwnedAppsandServices = $extractedSettings.UserOwnedAppsandServices
+      $settings.VivaLearning = $extractedSettings.VivaLearning
+      $settings.Whiteboard = $extractedSettings.Whiteboard
       
       return $settings
     }
