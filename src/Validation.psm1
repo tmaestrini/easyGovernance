cacd0e0a06f30f0cb1c487ef06dacb7c2705e277
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-05-23 16:29:24 +0200

    add utilities functions to module

--- src/Validation.psm1
+++ src/Validation.psm1
@@ -1,19 +1,25 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\Validation\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\Validation\*.ps1 -ErrorAction SilentlyContinue )
-$Utilities = @( Get-ChildItem -Path $PSScriptRoot\utilities\*.ps1 -ErrorAction SilentlyContinue )
+$Utilities = @( Get-ChildItem -Path $PSScriptRoot\utilities\*.psm1 -ErrorAction SilentlyContinue )
 
-Foreach ($import in @($Public + $Private + $Utilities)) {
+# Import all script resources
+Foreach ($import in @($Public + $Private)) {
   $import
   Try {
     . $import.fullname
   }
   Catch {
     Write-Error -Message "[Validation] Failed to import function $( $import.fullname ): $_"
+    Exit
   }
 }
 
 Export-ModuleMember -Function $Public.Basename
-Export-ModuleMember -Function $Utilities.Basename
+
+# Import all modules
+foreach ($module in $Utilities) {
+  Import-Module $module.FullName
+}
 
 # Check module dependencies before staring the routine
 Test-RequiredModules
