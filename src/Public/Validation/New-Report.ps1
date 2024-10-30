259aed9b2afabb2dd7b136e58c2774290378210e
Tobias Maestrini <tobias.maestrini@adesso.ch>
2024-10-30 22:10:11 +0100

    change attributes of csv file

--- src/Public/Validation/New-Report.ps1
+++ src/Public/Validation/New-Report.ps1
@@ -133,8 +133,8 @@ Function New-Report {
             return $data
          } 
       
-         $fileOutputPath = "$reportFilePath.csv"
-         $reportResultsPlain.Results | Export-Csv -Path $fileOutputPath -Encoding UTF8 -Delimiter ';'
+         $fileOutputPath = "$($reportPath)/$($ValidationResults.Tenant)-$($currentTimeStamp.toString("yyyyMMddHHmm")) report.csv"
+         $reportResultsPlain.Data | Export-Csv -Path $fileOutputPath -Encoding UTF8 -Delimiter ';'
          Write-Log -Level INFO -Message "CSV report created: $($fileOutputPath)"
       }
 
