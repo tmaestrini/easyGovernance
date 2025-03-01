4b82f9427e1f20583ac1fd88f29278c70c04fb6d
Daniel Kordes <daniel@dako365.com>
2025-03-01 11:08:16 +0100

    updated the necessary versions of the modules az.accounts and microsoft.graph to the latest versions

--- src/utilities/CommonFunctions.psm1
+++ src/utilities/CommonFunctions.psm1
@@ -39,8 +39,8 @@ Function Test-RequiredModules {
   $requiredModules = @(
     @{name = "powershell-yaml" }
     @{name = "PnP.PowerShell"; version = "2.12.0" }
-    @{name = "Microsoft.Graph"; version = "2.15.0" }
-    @{name = "Az.Accounts"; version = "2.19.0" }
+    @{name = "Microsoft.Graph"; version = "2.26.1" }
+    @{name = "Az.Accounts"; version = "4.0.2" }
     @{name = "Az.Resources"; version = "6.4.0" }
     @{name = "PSLogs"; version = "5.2.1" }
     @{name = "MarkdownPS"; version = "1.9" }
