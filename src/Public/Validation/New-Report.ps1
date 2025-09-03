8869bac5b9093ae2144623e0462b4f05e613801f
Tobias Maestrini <tobias.maestrini@gmail.com>
2025-09-03 10:04:13 +0200

    fix(report): remove TODO comment for guard clause in New-Report function

--- src/Public/Validation/New-Report.ps1
+++ src/Public/Validation/New-Report.ps1
@@ -22,7 +22,6 @@ Function New-Report {
    )
    
    Begin {
-      # TODO: Guard clause for empty results
       if (-not $ValidationResults -or -not $ValidationResults.Validation -or $ValidationResults.Validation.Count -eq 0) {
          Write-Log -Level WARNING "No validation results found; terminating report generation"
          continue
