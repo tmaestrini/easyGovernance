4d4bd14153fcbe54c319cefef24fc8c55eae8c76
Tobias Maestrini <tobias.maestrini@gmail.com>
2025-11-29 14:08:29 +0100

    refactor(validation): replace validation logic with Invoke-ValidationExecution in test functions

--- src/Private/Validation/Test-M365.1-1.2.ps1
+++ src/Private/Validation/Test-M365.1-1.2.ps1
@@ -98,12 +98,7 @@ Function Test-M365.1-1.2 {
   Process {
     try {
       $validator = [M365LicValidator]::new($Baseline, $tenantId, $ReturnAsObject)
-      $validator.StartValidation()
-      $result = $validator.GetValidationResult()
-      
-      if ($returnAsObject) {
-        return $result
-      }
+      Invoke-ValidationExecution -Validator $validator
     }
     catch {
       throw $_
