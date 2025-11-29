8b57bdce3f02fdb590aaaf2271e12ba897a67439
Tobias Maestrini <tobias.maestrini@gmail.com>
2025-11-29 12:56:24 +0100

    Revert "fix(validation): improve module import handling and error reporting for class imports"
    This reverts commit ce77847a49f155380e4acec94017678060c31c6e.

--- src/Validation.psm1
+++ src/Validation.psm1
@@ -3,23 +3,11 @@ $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\Validation\*.ps1 -ErrorA
 $Utilities = @( Get-ChildItem -Path $PSScriptRoot\utilities\*.psm1 -ErrorAction SilentlyContinue )
 $PublicCommon = @( Get-ChildItem -Path $PSScriptRoot\Public\*.psm1 -ErrorAction SilentlyContinue )
 
-$Classes = @( Get-ChildItem -Path $PSScriptRoot\Private\Validation\Class\*.psm1 -ErrorAction SilentlyContinue )
-
-Foreach ($class in $Classes) {
-  Try {
-    Import-Module $class.fullname -Global -Force -ErrorAction Stop
-  }
-  Catch {
-    Write-Error -Message "[Validation] Failed to import class $( $class.fullname ): $_"
-    Exit
-  }
-}
-
 # Import all script resources
 Foreach ($import in @($Public + $Private)) {
   $import
   Try {
-    Import-Module $import.fullname -Global -Force -ErrorAction Stop
+    . $import.fullname
   }
   Catch {
     Write-Error -Message "[Validation] Failed to import function $( $import.fullname ): $_"
