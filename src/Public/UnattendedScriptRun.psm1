4c0726948dfa7ee8a4bf8e3e8e85106ce387e425
Tobias Maestrini <tobias.maestrini@gmail.com>
2025-12-02 00:32:21 +0000

    feat(unattended): add optional azureSubscriptionId parameter to Set-UnattendedRun function

--- src/Public/UnattendedScriptRun.psm1
+++ src/Public/UnattendedScriptRun.psm1
@@ -1,13 +1,15 @@
 Function Set-UnattendedRun {
     param (
         [Parameter(Mandatory = $true)][string]$username,
-        [Parameter(Mandatory = $true)][SecureString]$password
+        [Parameter(Mandatory = $true)][SecureString]$password,
+        [Parameter(Mandatory = $false)][string]$azureSubscriptionId
     )
 
     [pscredential]$creds = New-Object System.Management.Automation.PSCredential ($username, $password)
 
     $Global:UnattendedScriptParameters = @{
         Credentials = $creds;
+        AzureSubscriptionId = $azureSubscriptionId ?? $null
     }
 }
 
